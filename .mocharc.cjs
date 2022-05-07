2bb0d4f1e95c711cc41f68131068bbc7b1120d4c
Matt Hughes <matt.c.hughes@gmail.com>
2022-05-07 09:42:58 -0600

    Add ESM support

---
+++ .mocharc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  "node-option": ["experimental-specifier-resolution=node", "loader=ts-node/esm", "no-warnings"],
+  spec: "test/**/*.ts",
+  "watch-files": ["test/**/*.ts", "src/**/*.ts"],
+};
