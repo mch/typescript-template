76ad95988ad56e8a43fb2ff5436d15724bc8eed4
Matt Hughes <matt.c.hughes@gmail.com>
2024-06-16 12:34:41 -0600

    Replace prettier with biome and format files

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,7 +1,7 @@
-import { expect } from "chai";
+import { expect } from 'chai'
 
-describe("something", () => {
-  it("does something", () => {
-    expect(true).is.true;
-  });
-});
+describe('something', () => {
+  it('does something', () => {
+    expect(true).is.true
+  })
+})
