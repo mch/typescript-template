bccc91bcd7c604e581d33a101d1198fcc3d631cb
Matt Hughes <matt.c.hughes@gmail.com>
2025-05-10 07:29:21 -0600

    Replace mocha and chai with vitest

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,7 +1,7 @@
-import { expect } from 'chai'
+import { describe, it, expect } from "vitest";
 
-describe('something', () => {
-  it('does something', () => {
-    expect(true).is.true
-  })
-})
+describe("something", () => {
+    it("does something", () => {
+        expect(true).is.true;
+    });
+});
